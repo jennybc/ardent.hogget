bbeee6724642abeb451fedfe0280f81c909d1460
Jenny Bryan <jenny.f.bryan@gmail.com>
2026-04-30 12:53:42 -0700

    Add, export, and document a function

---
+++ R/hello.R
@@ -0,0 +1,14 @@
+#' Say hello
+#'
+#' A simple greeting function.
+#'
+#' @param name A string. The name to greet.
+#'
+#' @returns A character string of the form `"Hello, {name}!"`.
+#' @export
+#'
+#' @examples
+#' hello("world")
+hello <- function(name = "world") {
+  paste0("Hello, ", name, "!")
+}
